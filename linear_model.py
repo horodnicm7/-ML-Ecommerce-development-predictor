eb54aff17c020962668b30cc493ed7f4000e3cd7
horodnicm7 <horodnicm7@gmail.com>
2019-05-19 13:16:45 +0300

    Fixed errors and approach

--- linear_model.py
+++ linear_model.py
@@ -2,9 +2,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import warnings
+
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_squared_log_error, median_absolute_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import mean_squared_log_error, median_absolute_error
+from sklearn.metrics import explained_variance_score
+
+
+# suppress warning thrown by seaborn
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 customers = pd.read_csv('Ecommerce Customers.csv')
 
@@ -24,19 +32,13 @@ customers = pd.read_csv('Ecommerce Customers.csv')
 
 
 # plot data and draw a regression line
-sns.lmplot('Length of Membership', 'Yearly Amount Spent', customers)
+#sns.lmplot('Length of Membership', 'Yearly Amount Spent', customers)
 
-X = customers['Length of Membership']
+X = customers[['Length of Membership', 'Time on App', 'Time on Website', 'Avg. Session Length']]
 Y = customers['Yearly Amount Spent']
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3, 
                                                     random_state = 101)
-
-# reshape data to be a len x 1 matrix
-x_train = np.array(x_train).reshape((-1, 1))
-y_train = np.array(y_train).reshape((-1, 1))
-x_test = np.array(x_test).reshape((-1, 1))
-y_test = np.array(y_test).reshape((-1, 1))
 
 # declare and train model
 model = LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1, normalize=False)
@@ -53,16 +55,25 @@ MEAE = median_absolute_error(y_test, result)
 print("MSLE (mean squared log error): {}".format(MSLE))
 print("MEAE (median absolute error): {}".format(MEAE))
 print("MAE (mean absolute error): {}".format(MAE))
-print("MSE (mean squared error): {}".format(MSE))
+print("RMSE (sqrt of mean squared error): {}".format(np.sqrt(MSE)))
+print("MSE (mean squared error): {}\n".format(MSE))
 
-# plot correct and predicted points
-fig = plt.figure()
-axis = fig.add_subplot(111)
-axis.scatter(x_test.reshape((1, -1)).tolist()[0], y_test.reshape((1, -1)).tolist()[0], label='correct', s=16)
-axis.scatter(x_test.reshape((1, -1)).tolist()[0], result.reshape((1, -1)).tolist()[0], c='r', label='predicted', s=16)
+# a coefficient means that for every unit your data is increased with, the 
+# output will be increased with the coefficient
+coefficients = pd.DataFrame(model.coef_, x_train.columns, columns=['Coefficient'])
+print(coefficients)
+
+# it should be as high as possible for a good model
+print("\nVariance score: {}".format(explained_variance_score(y_test, result)))
+
+# plot difference between correct and predicted values
+sns.distplot((y_test - result), bins=50)
+plt.show() # force the above plot to be shown
+
+# plot data. The output should form a linear equation as much as possible
+plt.scatter(y_test, result, s=16)
 
 plt.title('Correct vs predicted values')
-plt.xlabel('Length of Membership')
-plt.ylabel('Yearly Amount Spent')
-fig.legend(loc='upper left')
+plt.xlabel('Test output')
+plt.ylabel('Predicted output')
 plt.show()
